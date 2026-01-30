1dcae8b7f726d63f4a327c388ec4a2e36d46f414
hundessa999 <nhundessa@gmail.com>
2026-01-30 11:01:11 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,9 @@
-export default function Home(){return <h1>Reelforge</h1>}
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Reelforge</h1>
+      <p>AI Reels Ad Generator</p>
+      <a href="/generate">Generate Ads</a>
+    </main>
+  );
+}
